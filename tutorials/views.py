3a79a9133de4c5f09489c3facf8f6e7d5d17c5b3
paulwababu <paulkiragu621@gmail.com>
2021-03-13 17:21:20 +0300

    Added more HTML files

--- tutorials/views.py
+++ tutorials/views.py
@@ -56,3 +56,83 @@ def deleteTutorial(request, pk):
 def signout(request):
     logout(request)
     return redirect('/')
+
+def about(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/about-us.html', { 'tutorials' : tutorials})    
+
+def developingteamcreativityinitiative(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/developing-team-creativity-initiative.html', { 'tutorials' : tutorials})
+
+def leadership_development_skills(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/leadership_development_skills.html', { 'tutorials' : tutorials})
+
+def project_team_leadership(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/project_team_leadership.html', { 'tutorials' : tutorials})
+
+def coaching_a_service_team(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/coaching_a_service_team.html', { 'tutorials' : tutorials})
+
+def the_voice_of_leadership(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/the_voice_of_leadership.html', { 'tutorials' : tutorials})
+
+def creating_leaders_through_mentorship(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/creating_leaders_through_mentorship.html', { 'tutorials' : tutorials})
+
+def leading_change_organizational_renewal(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/leading_change_organizational_renewal.html', { 'tutorials' : tutorials})
+
+def high_impact_executive_program(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/high_impact_executive_program.html', { 'tutorials' : tutorials})
+
+def achieving_leadership_success_through_people(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/achieving_leadership_success_through_people.html', { 'tutorials' : tutorials})
+
+def leading_with_emotional_intelligence(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/leading_with_emotional_intelligence.html', { 'tutorials' : tutorials})
+
+def attitudes_for_customer_service(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/attitudes_for_customer_service.html', { 'tutorials' : tutorials})
+
+def building_customer_loyalty(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/building_customer_loyalty.html', { 'tutorials' : tutorials})
+
+def call_centre_and_customer_care_skills(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/call_centre_and_customer_care_skills.html', { 'tutorials' : tutorials})
+
+def customer_service_excellence_for_managers(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/customer_service_excellence_for_managers.html', { 'tutorials' : tutorials})
+
+def how_to_exceed_customers_expectations(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/how_to_exceed_customers_expectations.html', { 'tutorials' : tutorials})
+
+def mastering_the_telephone(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/mastering_the_telephone.html', { 'tutorials' : tutorials})
+
+def ultimate_loyalty_build_teams_that_wow_the_customer(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/ultimate_loyalty_build_teams_that_wow_the_customer.html', { 'tutorials' : tutorials})
+
+def strategies_of_dealing_with_dissatisfied_customers(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/strategies_of_dealing_with_dissatisfied_customers.html', { 'tutorials' : tutorials})
+
+def managing_conflict_at_work_place(request):
+    tutorials = Tutorial.objects.all()
+    return render(request, 'tutorial/managing_conflict_at_work_place.html', { 'tutorials' : tutorials})
