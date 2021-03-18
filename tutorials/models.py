a5f94fd01150dd7c909b68654f71c833e63d4752
paulwababu <44939805+paulwababu@users.noreply.github.com>
2021-03-18 10:26:19 +0300

    Update models.py

--- tutorials/models.py
+++ tutorials/models.py
@@ -33,3 +33,14 @@ class Tutorial(models.Model):
         self.feature_image.delete()
         self.attachment.delete()
         super().delete(*args, **kwargs)
+
+class Monitor(models.Model):
+    continent = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    capital = models.CharField(max_length=50, blank=True, null=True)
+    datetime = models.CharField(max_length=50, blank=True, null=True)
+    ip = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.ip
